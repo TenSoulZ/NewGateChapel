db1a846d1d8d7c9edf0c27b58cca72725d527eb9
Tafadzwa Emmanuel Njagu <mcnashten@gmail.com>
2026-02-01 15:15:33 +0200

    Automate superuser creation

--- backend/build.sh
+++ backend/build.sh
@@ -10,3 +10,8 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
+
+# Create superuser (fail silently if exists)
+if [[ -n "$DJANGO_SUPERUSER_USERNAME" ]]; then
+    python manage.py createsuperuser --noinput || echo "Superuser already exists or creation failed."
+fi
